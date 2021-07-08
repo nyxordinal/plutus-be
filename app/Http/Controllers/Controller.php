f0eb71b4c0e9c79b2736c079b4873805f6932645
Nyxordinal <nyxordinal@gmail.com>
2021-07-08 17:25:01 +0700

    modify controller response helper and activate facade eloquent

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Response;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
@@ -25,14 +26,14 @@ class Controller extends BaseController
         return $this->baseResponse(Response::HTTP_CREATED, $message, false, $data);
     }
 
-    public function internalServerErrorResponse(\Exception $exception, $message = 'Unexpected Error')
+    public function internalServerErrorResponse(Exception $exception, $message = 'Unexpected Error')
     {
         return $this->baseResponse(Response::HTTP_INTERNAL_SERVER_ERROR, $message, true, null, $exception->getMessage());
     }
 
-    public function badRequestResponse(\Exception $exception, $message = 'Bad Request')
+    public function badRequestResponse($errors, $message = 'Bad Request')
     {
-        return $this->baseResponse(Response::HTTP_BAD_REQUEST, $message, true, null, $exception->getMessage());
+        return $this->baseResponse(Response::HTTP_BAD_REQUEST, $message, true, null, $errors);
     }
 
     public function unauthorizedResponse($message = 'Unauthorized')
