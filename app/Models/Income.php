823b73ea7b453dff3d7010c80955ec450a34e95e
Nyxordinal <kujyokazunejpn@gmail.com>
2021-07-08 23:14:01 +0700

    add fillable in income

--- app/Models/Income.php
+++ app/Models/Income.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Income extends Model
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['source', 'amount', 'date'];
+
+    /**
      * Get the user that owns the income.
      */
     public function user()
