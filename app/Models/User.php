c14e42599f3553a3f34908e91e482508b8e4ba1f
Nyxordinal <kujyokazunejpn@gmail.com>
2021-08-31 17:45:59 +0700

    move auth token to response header

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,7 +29,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $hidden = [
-        'password', "created_at", "updated_at", "id"
+        'password', "created_at", "updated_at"
     ];
 
     /**
